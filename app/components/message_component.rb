04a72762474c695f23c1ff9116a1a34b948025b4
MaratRakhimov <mf.rakhimov1982@gmail.com>
2025-01-19 11:44:46 +0300

    Update Message component

--- app/components/message_component.rb
+++ app/components/message_component.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class MessageComponent < ViewComponent::Base
+  include ActionView::RecordIdentifier
+
+  def initialize(message:)
+    @message = message
+  end
+
+  private
+
+  attr_reader :message
+
+  delegate :body, :created_at, to: :message, prefix: true
+
+  def recent_message?
+    message_created_at > 1.hour.age
+  end
+
+  def timestamp
+    message_created_at.strftime("%Y-%m-%d at %H:%M")
+  end
 end
