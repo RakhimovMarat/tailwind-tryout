7120388e66ab7276ecdefd82a6fbef28289e4989
MaratRakhimov <mf.rakhimov1982@gmail.com>
2025-01-19 13:15:16 +0300

    Add Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_25_180057) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_19_101256) do
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
