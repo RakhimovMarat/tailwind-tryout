e04c23dda70442ac6d16118b3bf186cbe87a69e3
MaratRakhimov <mf.rakhimov1982@gmail.com>
2025-01-26 21:21:33 +0300

    Fixing misprint in stimulus controller

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
-  static target = [ "source" ]
+  static targets = [ "source" ]
 
   copy_old() {
     navigator.clipboard.writeText(this.sourceTarget.value)
